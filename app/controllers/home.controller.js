d75bfe9d6bee7d3251bf495b20e24d5e247c2c4b
Tyler Lam <tyler.lam@wishabi.com>
2017-02-04 21:24:54 -0500

    name and game mode shit added

--- app/controllers/home.controller.js
+++ app/controllers/home.controller.js
@@ -6,10 +6,25 @@ HomeCtrl.$inject = ['$scope', 'Home'];
 function HomeCtrl($scope, Home) {
   $scope.users = Home.all();
 
+  $scope.easyMode = function () {
+  	if ($('#easy-button').hasClass('active') == false) {
+  		$('#easy-button').addClass('active');
+  		$('#hard-button').removeClass('active');
+  	}
+  }
+  $scope.hardMode = function () {
+  	$('#easy-button').removeClass('active');
+  	$('#hard-button').addClass('active');
+  }
+
   $scope.beginGame = function () {
-  	console.log("hello");
+  	//console.log("hello");
   	$('#myModal').modal('hide');
 	$('body').removeClass('modal-open');
 	$('.modal-backdrop').remove();
+	var gameMode = $('#game-mode .active').attr('value');
+	console.log(gameMode);
+	var name = document.getElementById('1234').value;
+	console.log(name);
   }
 }
