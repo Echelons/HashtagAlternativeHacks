ea7dff138494d57a8caa6ba2aad7b1d3ea97813d
Hunter Derkach <hunterderkach@gmail.com>
2017-02-04 22:26:44 -0500

    added game functionality to home. YUGE PIVOT

--- app/controllers/home.controller.js
+++ app/controllers/home.controller.js
@@ -27,4 +27,54 @@ function HomeCtrl($scope, Home) {
 	var name = document.getElementById('1234').value;
 	console.log(name);
   }
+
+  $scope.points = 0;
+
+  client = new Paho.MQTT.Client('m13.cloudmqtt.com', 33244, "web_" + parseInt(Math.random() * 100, 10));
+
+  // set callback handlers
+  client.onConnectionLost = onConnectionLost;
+  client.onMessageArrived = onMessageArrived;
+
+  var opts = {
+    useSSL: true,
+    userName: "front",
+    password: "qhacks",
+    onSuccess: onConnect
+  };
+
+  // connect the client
+  client.connect(opts);
+
+  function onConnect() {
+    console.log("onConnect");
+    client.subscribe("/qhacks/alternativehacks");
+  }
+
+  // called when the client loses its connection
+  function onConnectionLost(responseObject) {
+    console.log("Connection Lost. Reconnecting...");
+    client.connect(opts);
+  }
+
+  // called when a message arrives
+  function onMessageArrived(message) {
+    var response = JSON.parse(message.payloadString);
+    if(response.message == "debug") {
+      console.log("Debugging");
+    } else if (response.message == "gameready") {
+      console.log("Game Ready");
+      $scope.points = 0;
+    } else if (response.message == "score") {
+      console.log("Score");
+      $scope.points += response.points;
+      $scope.$apply();
+    } else if (response.message == "gamecomplete") {
+      console.log("Game Complete");
+      $scope.points = 0;
+    } else {
+
+    }
+  };
+
 }
