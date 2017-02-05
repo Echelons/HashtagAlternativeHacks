103524b4a0abf6459bcbe336dc35a4717495d954
Tyler Lam <tyler.lam@wishabi.com>
2017-02-05 01:59:13 -0500

    added timer

--- app/controllers/home.controller.js
+++ app/controllers/home.controller.js
@@ -6,26 +6,42 @@ HomeCtrl.$inject = ['$scope', 'Home'];
 function HomeCtrl($scope, Home) {
   $scope.users = Home.all();
 
-  $scope.easyMode = function () {
-  	if ($('#easy-button').hasClass('active') == false) {
-  		$('#easy-button').addClass('active');
-  		$('#hard-button').removeClass('active');
-  	}
-  }
-  $scope.hardMode = function () {
-  	$('#easy-button').removeClass('active');
-  	$('#hard-button').addClass('active');
-  }
+ //  $scope.easyMode = function () {
+ //  	if ($('#easy-button').hasClass('active') == false) {
+ //  		$('#easy-button').addClass('active');
+ //  		$('#hard-button').removeClass('active');
+ //  	}
+ //  }
+ //  $scope.hardMode = function () {
+ //  	$('#easy-button').removeClass('active');
+ //  	$('#hard-button').addClass('active');
+ //  }
 
-  $scope.beginGame = function () {
-  	//console.log("hello");
-  	$('#myModal').modal('hide');
-	$('body').removeClass('modal-open');
-	$('.modal-backdrop').remove();
-	var gameMode = $('#game-mode .active').attr('value');
-	console.log(gameMode);
-	var name = document.getElementById('1234').value;
-	console.log(name);
+ //  $scope.beginGame = function () {
+ //  	//console.log("hello");
+ //  	$('#myModal').modal('hide');
+	// $('body').removeClass('modal-open');
+	// $('.modal-backdrop').remove();
+	// var gameMode = $('#game-mode .active').attr('value');
+	// console.log(gameMode);
+	// var name = document.getElementById('1234').value;
+	// console.log(name);
+ //  }
+  
+  var startTime;
+  var currentTime;
+
+  $scope.t = 0;
+
+  $scope.startTimer = function () {
+    startTime = moment();
+    window.setInterval(function () {
+      currentTime = moment();
+      // currentTime /= 1000;
+      // var seconds = Math.round(currentTime % 60);
+      $scope.t = moment(currentTime.diff(startTime)).format("mm:ss");
+      $scope.$apply();
+    }, 1000)
   }
 
   $scope.points = 0;
